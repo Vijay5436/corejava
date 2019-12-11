c21d67d3943ac8e23950d69ef82e9886fb2706e7
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2019-12-11 17:59:10 +0530

    Add files via upload

---
+++ unBoxing.java
@@ -0,0 +1,10 @@
+package Wapper;
+
+public class unBoxing {
+public static void main(String args[])
+{Integer a=new Integer(10);
+int i=a.intValue();
+int j=i;
+System.out.println(a+" "+i+" "+j);
+}
+}
