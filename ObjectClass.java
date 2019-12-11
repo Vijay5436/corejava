86e9e3873acd3bedeb8d3ad83d88619f84a4859a
Vijay5436 <58158521+Vijay5436@users.noreply.github.com>
2019-12-11 15:46:55 +0530

    Add files via upload

---
+++ ObjectClass.java
@@ -0,0 +1,40 @@
+package inheritance;
+
+public class ObjectClass {
+	static int last_roll=100;
+	int roll_no;
+	
+	//Constructor
+ObjectClass()
+{
+	roll_no=last_roll;
+	last_roll++;
+}
+
+//Overriding hashCode()
+@Override
+public int hashCode()
+{
+	return roll_no;
+}
+//Driver code
+public static void main(String args[])
+{
+	ObjectClass s=new ObjectClass();
+	//Bellow two statements are equivalent
+	System.out.println(s);
+	System.out.println(s.toString());
+	Object obj=new String("mass");
+	Class c=obj.getClass();
+	System.out.println("Class of object is : "+c.getName());
+	s=null;
+	System.gc();
+}
+@Override
+protected void finalize()
+{
+	System.out.println("finalized method class");
+	
+			
+}
+}
